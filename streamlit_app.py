d64c971cdd99009fd854023cf0dd8e16a67b5478
KristalDuran <31260047+KristalDuran@users.noreply.github.com>
2025-06-12 21:07:26 -0600

    Update streamlit_app.py
    fds

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,7 @@ name = st.text_input("Name on Smoothie", "")
 
 cnx = st.connection("snowflake")
 session = cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
 # st.dataframe(data=my_dataframe, use_container_width=True)
 
 ingredients_list = st.multiselect(
@@ -31,6 +31,7 @@ if len(ingredients_list) <= 5:
         st.subheader(fruit_chosen+' Nutrition info')
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/"+fruit_chosen)
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+        st.stop()
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
             values ('""" + ingredients_string + """', '""" + name + """')"""
 
